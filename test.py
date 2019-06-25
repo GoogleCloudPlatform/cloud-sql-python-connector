8b462be4a35584ea05a8fc1c1a0f1d8984e46c35
Ryan Chen <ryachen@google.com>
2019-06-25 11:22:42 -0700

    Added Python3.6 to travis

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import pytest  # noqa: F401; pylint: disable=unused-variable
+from utils import generate_keys
 
 
+# Test to make sure there isn't a fatal error with Python
 def test1():
     assert 1 == 1
+
+
+def test2():
+    res1, res2 = generate_keys()
+    assert (res1 is not None) and (res2 is not None)
