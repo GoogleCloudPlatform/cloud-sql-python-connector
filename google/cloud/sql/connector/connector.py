e49057c67a08b51d45969e390299bc342cf6395c
Ryan Chen <ryachen@google.com>
2019-07-03 10:16:39 -0700

    Adding get_metadata function (#5)
    * Created request-metadata function and added documentation.
    
    * Added in rest of get_metadata function
    
    * Changed get_metadata.py's return value to include a dictionary of ipAddresses
    
    * Un-abbreviated project and instance.
    
    * Took out docstring line talking about Cloud SQL instance name environment variable
    
    * Added return types to docstrings.
    
    * Changed return type in get_metadata docstring

--- google/cloud/sql/connector/connector.py
+++ google/cloud/sql/connector/connector.py
@@ -17,7 +17,55 @@ limitations under the License.
 import googleapiclient
 
 
-def get_ephemeral(service, proj_name, inst_name, pub_key):
+def get_metadata(service, project, instance):
+    """
+    A helper function that requests metadata from the Cloud SQL Instance
+    and returns a dictionary containing the IP addresses and certificate
+    authority of the Cloud SQL Instance.
+
+    Args:
+        service (googleapiclient.discovery.Resource): A service object created
+          from the Google Python API client library. Must be using the SQL
+          Admin API. For more info check out
+          https://github.com/googleapis/google-api-python-client.
+        proj_name (str): A string representing the name of the project.
+        inst_name(str): A string representing the name of the instance.
+
+    Returns:
+          Dict[str: Union[Dict, str]]
+          Returns a dictionary containing a dictionary of all IP addresses and
+          their type and a string representing the certificate authority.
+
+    Raises:
+        TypeError: If any of the arguments are not the specified type.
+    """
+
+    if (
+        not isinstance(service, googleapiclient.discovery.Resource)
+        or not isinstance(project, str)
+        or not isinstance(instance, str)
+    ):
+        raise TypeError(
+            "Arguments must be as follows: "
+            + "service (googleapiclient.discovery.Resource), "
+            + "proj_name (str) and inst_name (str)."
+        )
+
+    req = service.instances().get(project=project, instance=instance)
+    res = req.execute()
+
+    # Extract server certificate authority
+    serverCaCert = res["serverCaCert"]["cert"]
+
+    # Map IP addresses to type.
+    ip_map = {ip["type"]: ip["ipAddress"] for ip in res["ipAddresses"]}
+
+    metadata = {"ip_addresses": ip_map, "server_ca_cert": serverCaCert}
+
+    return metadata
+
+
+def get_ephemeral(service, project, instance, pub_key):
     """
     A helper function that requests an ephemeral certificate from the
     Cloud SQL Instance.
@@ -29,12 +77,12 @@ def get_ephemeral(service, proj_name, inst_name, pub_key):
           https://github.com/googleapis/google-api-python-client.
         project (str): A string representing the name of the project.
         instance (str): A string representing the name of the instance.
-            Usually found in environment variable 'CLOUD_SQL_INSTANCE_NAME.'
         pub_key (str): A string representing PEM-encoded RSA public key.
 
     Returns:
-        An ephemeral certificate from the Cloud SQL instance that allows
-        authorized connections to the instance.
+        str
+          An ephemeral certificate from the Cloud SQL instance that allows
+          authorized connections to the instance.
 
     Raises:
         TypeError: If one of the arguments passed in is None.
@@ -42,8 +90,8 @@ def get_ephemeral(service, proj_name, inst_name, pub_key):
 
     if (
         not isinstance(service, googleapiclient.discovery.Resource)
-        or not isinstance(proj_name, str)
-        or not isinstance(inst_name, str)
+        or not isinstance(project, str)
+        or not isinstance(instance, str)
         or not isinstance(pub_key, str)
     ):
         raise TypeError("Cannot take None as an argument.")
@@ -51,7 +99,7 @@ def get_ephemeral(service, proj_name, inst_name, pub_key):
     # TODO(ryachen@) Add checks to ensure service object is valid.
 
     request = service.sslCerts().createEphemeral(
-        project=proj_name, instance=inst_name, body={"public_key": pub_key}
+        project=project, instance=instance, body={"public_key": pub_key}
     )
     response = request.execute()
 
