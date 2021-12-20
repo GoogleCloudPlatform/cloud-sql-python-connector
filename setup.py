a2d7df1071dfea8e7154589dba3e5d46c158abdd
jackwotherspoon <jackwoth@google.com>
2021-12-20 15:12:12 +0000

    chore: add python 3.10 to setup.py

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     platforms="Posix; MacOS X; Windows",
     packages=packages,
