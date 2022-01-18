cda8f6c1396253035766d5bd45f0e19d773ed41c
jackwotherspoon <jackwoth@google.com>
2022-01-18 15:22:55 +0000

    chore: add asyncpg dependency

--- setup.py
+++ setup.py
@@ -80,7 +80,8 @@ setup(
     extras_require={
         "pymysql": ["PyMySQL==1.0.2"],
         "pg8000": ["pg8000==1.23.0"],
-        "pytds": ["python-tds==1.11.0"]
+        "pytds": ["python-tds==1.11.0"],
+        "asyncpg": ["asyncpg==0.25.0"],
     },
     python_requires=">=3.6",
     include_package_data=True,
