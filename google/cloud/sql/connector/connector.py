85bd36e29fb9d0d29b44a33a05389d6e602f3192
jackwotherspoon <jackwoth@google.com>
2022-01-27 14:54:20 +0000

    chore: update warning message

--- google/cloud/sql/connector/connector.py
+++ google/cloud/sql/connector/connector.py
@@ -126,8 +126,8 @@ class Connector:
         if "ip_types" in kwargs:
             ip_type = kwargs.pop("ip_types")
             logger.warning(
-                "Deprecation Warning: Parameter `ip_types` is deprecated and may be removed "
-                "in a future release. Please use `ip_type` instead."
+                "Deprecation Warning: Parameter `ip_types` is deprecated and may be removed"
+                " in a future release. Please use `ip_type` instead."
             )
         else:
             ip_type = kwargs.pop("ip_type", self._ip_type)
