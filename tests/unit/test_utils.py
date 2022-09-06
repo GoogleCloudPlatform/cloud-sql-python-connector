bf50b819421b2fe44f80413b60d9556219ab845c
jackwotherspoon <jackwoth@google.com>
2022-09-06 00:41:54 +0000

    chore: add tests for new functions

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -39,3 +39,65 @@ async def test_generate_keys_returns_bytes_and_str() -> None:
 
     res1, res2 = await utils.generate_keys()
     assert isinstance(res1, bytes) and (isinstance(res2, str))
+
+
+def test_remove_suffix_with_suffix() -> None:
+    """
+    Test to check if remove_suffix returns string with suffix removed.
+    """
+    output = utils.remove_suffix(
+        "service-account@test.iam.gserviceaccount.com", ".gserviceaccount.com"
+    )
+    assert output == "service-account@test.iam"
+
+
+def test_remove_suffix_without_suffix() -> None:
+    """
+    Test to check if remove_suffix returns input string if suffix not present.
+    """
+    output = utils.remove_suffix("service-account@test.iam", ".gserviceaccount.com")
+    assert output == "service-account@test.iam"
+
+
+def test_validate_database_user_postgres() -> None:
+    """
+    Test that validate_database user throws no exception for properly
+    formatted Postgres database users.
+    """
+    utils.validate_database_user("POSTGRES_14", "service-account@test.iam")
+    utils.validate_database_user("POSTGRES_14", "test@test.com")
+
+
+def test_validate_database_user_mysql() -> None:
+    """
+    Test that validate_database user throws no exception for properly
+    formatted MySQL database users.
+    """
+    utils.validate_database_user("MYSQL_8_0", "service-account")
+    utils.validate_database_user("MYSQL_8_0", "test")
+
+
+def test_validate_database_user_InvalidPostgresDatabaseUser() -> None:
+    """
+    Test that validate_database user raises exception with improperly
+    formatted Postgres service account database user.
+    """
+    with pytest.raises(utils.InvalidPostgresDatabaseUser):
+        utils.validate_database_user(
+            "POSTGRES_14", "service-account@test.iam.gserviceaccount.com"
+        )
+
+
+def test_validate_database_user_InvalidMySQLDatabaseUser() -> None:
+    """
+    Test that validate_database user raises exception with improperly
+    formatted MySQL database user.
+    """
+    # test IAM service account user
+    with pytest.raises(utils.InvalidMySQLDatabaseUser):
+        utils.validate_database_user(
+            "MYSQL_8_0", "service-account@test.iam.gserviceaccount.com"
+        )
+    # test IAM user
+    with pytest.raises(utils.InvalidMySQLDatabaseUser):
+        utils.validate_database_user("MYSQL_8_0", "test@test.com")
