e546efbff967cd6880d2238ba4be2f8711952562
Jack Wotherspoon <jackwoth@google.com>
2023-02-07 10:09:44 -0500

    feat: improve reliability of certificate refresh (#599)

--- tests/unit/test_refresh_utils.py
+++ tests/unit/test_refresh_utils.py
@@ -1,4 +1,4 @@
-""""
+"""
 Copyright 2021 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from typing import Any, no_type_check
-
+from datetime import datetime, timedelta
 import aiohttp
 from google.auth.credentials import Credentials
 import google.oauth2.credentials
@@ -28,6 +28,7 @@ from google.cloud.sql.connector.refresh_utils import (
     _get_metadata,
     _is_valid,
     _downscope_credentials,
+    _seconds_until_refresh,
 )
 from google.cloud.sql.connector.utils import generate_keys
 
@@ -37,7 +38,7 @@ from mocks import (  # type: ignore
     instance_metadata_expired,
     FakeCSQLInstance,
 )
-from tests.conftest import SCOPES  # type: ignore
+from conftest import SCOPES  # type: ignore
 
 
 @pytest.fixture
@@ -265,3 +266,39 @@ def test_downscope_credentials_user() -> None:
     # verify downscoped credentials have new scope
     assert credentials.scopes == ["https://www.googleapis.com/auth/sqlservice.login"]
     assert credentials != creds
+
+
+def test_seconds_until_refresh_over_1_hour() -> None:
+    """
+    Test _seconds_until_refresh returns proper time in seconds.
+
+    If expiration is over 1 hour, should return duration/2.
+    """
+    # using pytest.approx since sometimes can be off by a second
+    assert (
+        pytest.approx(_seconds_until_refresh(datetime.now() + timedelta(minutes=62)), 1)
+        == 31 * 60
+    )
+
+
+def test_seconds_until_refresh_under_1_hour_over_4_mins() -> None:
+    """
+    Test _seconds_until_refresh returns proper time in seconds.
+
+    If expiration is under 1 hour and over 4 minutes,
+    should return duration-refresh_buffer (refresh_buffer = 4 minutes).
+    """
+    # using pytest.approx since sometimes can be off by a second
+    assert (
+        pytest.approx(_seconds_until_refresh(datetime.now() + timedelta(minutes=5)), 1)
+        == 60
+    )
+
+
+def test_seconds_until_refresh_under_4_mins() -> None:
+    """
+    Test _seconds_until_refresh returns proper time in seconds.
+
+    If expiration is under 4 minutes, should return 0.
+    """
+    assert _seconds_until_refresh(datetime.now() + timedelta(minutes=3)) == 0
