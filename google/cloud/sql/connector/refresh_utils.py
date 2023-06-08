a343454c2b4183082ef86471f5d5a45b6d509a8c
jackwotherspoon <jackwoth@google.com>
2023-06-08 13:57:55 +0000

    chore: add TYPE_CHECKING for imports

--- google/cloud/sql/connector/refresh_utils.py
+++ google/cloud/sql/connector/refresh_utils.py
@@ -13,17 +13,19 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+from __future__ import annotations
+
 import asyncio
 import copy
 import datetime
 import logging
-from typing import Any, Dict, List
+from typing import Any, Dict, List, TYPE_CHECKING
 
-import aiohttp
-
-import google.auth
 from google.auth.credentials import Credentials, Scoped
 import google.auth.transport.requests
+
+if TYPE_CHECKING:
+    import aiohttp
 
 logger = logging.getLogger(name=__name__)
 
