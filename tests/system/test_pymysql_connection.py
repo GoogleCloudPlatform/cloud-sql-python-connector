07fb11353d9e3b6d9a74f8bfcf20d1d30ce69f2f
kgala2 <galakp@google.com>
2025-04-02 16:11:32 -0700

    chore: add a broken assert

--- tests/system/test_pymysql_connection.py
+++ tests/system/test_pymysql_connection.py
@@ -143,3 +143,7 @@ def test_MCP_pymysql_connection() -> None:
         curr_time = time[0]
         assert type(curr_time) is datetime
     connector.close()
+
+def test_broken_test_reports() -> None:
+    '''This test breaks but that's expected'''
+    assert 1 == 2
