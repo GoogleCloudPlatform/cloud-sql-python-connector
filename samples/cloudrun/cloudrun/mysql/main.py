fdbccc8e9edfa0c228aaf7a547d042b2485de818
kgala2 <galakp@google.com>
2025-11-20 08:30:12 +0000

    chore: update license and exception

--- samples/cloudrun/cloudrun/mysql/main.py
+++ samples/cloudrun/cloudrun/mysql/main.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import os
 import sqlalchemy
 from flask import Flask
@@ -68,7 +84,7 @@ def password_auth_index():
             result = conn.execute(sqlalchemy.text("SELECT 1")).fetchall()
             return f"Database connection successful (password authentication), result: {result}"
     except Exception as e:
-        return f"Error connecting to the database (password authentication): {e}", 500
+        return f"Error connecting to the database (password authentication)", 500
 
 @app.route("/iam")
 def iam_auth_index():
@@ -77,7 +93,7 @@ def iam_auth_index():
             result = conn.execute(sqlalchemy.text("SELECT 1")).fetchall()
             return f"Database connection successful (IAM authentication), result: {result}"
     except Exception as e:
-        return f"Error connecting to the database (IAM authentication): {e}", 500
+        return f"Error connecting to the database (IAM authentication)", 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- samples/cloudrun/cloudrun/sqlserver/main.py
+++ samples/cloudrun/cloudrun/sqlserver/main.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import os
 import sqlalchemy
 from flask import Flask
@@ -51,7 +67,7 @@ def index():
             result = conn.execute(sqlalchemy.text("SELECT 1")).fetchall()
             return f"Database connection successful, result: {result}"
     except Exception as e:
-        return f"Error connecting to the database: {e}", 500
+        return f"Error connecting to the database", 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
